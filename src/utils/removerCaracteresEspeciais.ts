1f0b14be44f45860d83eccaa4fe3f153ed1a6bca
xuniorss <fortunatojunior2020@gmail.com>
2024-07-05 16:59:47 -0300

    fix: corrigindo fn

--- src/utils/removerCaracteresEspeciais.ts
+++ src/utils/removerCaracteresEspeciais.ts
@@ -1,18 +1,13 @@
 export const removerCaracteresEspeciais = (texto: string): string => {
-	let textoSemEspacos = texto.replace(/\s+/g, '-')
-
-	textoSemEspacos = textoSemEspacos.replace(/-{2,}/g, '-')
+	let textoSemEspacos = texto.replace(/[\s_]+/g, '-')
 
 	textoSemEspacos = textoSemEspacos
 		.normalize('NFD')
 		.replace(/[\u0300-\u036f]/g, '')
-		.replace(/[çÇ]/g, 'c')
 
-	textoSemEspacos = textoSemEspacos.replace(/\//g, '-')
-
-	const regex = /[^a-zA-Z0-9/-]/g
-
-	textoSemEspacos = textoSemEspacos.replace(regex, '')
+	textoSemEspacos = textoSemEspacos.replace(/[^a-zA-Z0-9-]/g, '-')
+	textoSemEspacos = textoSemEspacos.replace(/-{2,}/g, '-')
+	textoSemEspacos = textoSemEspacos.replace(/^-+|-+$/g, '')
 
 	return textoSemEspacos
 }
